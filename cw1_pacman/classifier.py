14faa3e8a33d7e373fc6fabda41709138a28032a
Killian <killian2100@hotmail.com>
2023-02-13 12:33:16 +0000

    Added *.pyc to .gitignore & Added Comments to classifier.py
    Team should read comments in classifier.py. I've tried to familiarise myself with its interactions with classifierAgent.py

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -4,15 +4,32 @@
 # Use the skeleton below for the classifier and insert your code here.
 
 class Classifier:
+    # Classifier is initialised with nothing passed through. Only initialise 
+    # object scope constants and variables here. We may not even use this.
     def __init__(self):
         pass
 
+    # As far as I can see, we don't have to perform any retraining on the model
+    # between sets of games, unless we know the environment changes (I believe
+    # it doesn't). Again we may not even use this.
     def reset(self):
         pass
     
+    # Trains on training dataset, the arrays of input <data>, on target, or 
+    # output singular integer <target>.
+    #
+    # This happens before classifier is run on the environment.
     def fit(self, data, target):
+        
         pass
 
+    # We simply output the integer output/prediction given a singular feature
+    # array. Current legal moves are given as array of string 'North', 'East',
+    # 'South', 'West', and 'Stop'.
+    #
+    # ClassifierAgent.py handles output conversion of the number to these 
+    # strings taken by the environment. So we may not even use legal at all.
     def predict(self, data, legal=None):
+        print(legal)
         return 1
         
