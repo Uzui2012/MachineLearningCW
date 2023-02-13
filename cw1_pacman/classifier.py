ea850fdeee0b36f68908ddf6bfe58e918ba8e7e4
Killian <killian2100@hotmail.com>
2023-02-13 17:32:50 +0000

    Comments Update

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -2,11 +2,14 @@
 # Lin Li/26-dec-2021
 #
 # Use the skeleton below for the classifier and insert your code here.
+from sklearn.neighbors import KNeighborsClassifier
+
 
 class Classifier:
     # Classifier is initialised with nothing passed through. Only initialise 
     # object scope constants and variables here. We may not even use this.
     def __init__(self):
+        self.knn = KNeighborsClassifier(n_neighbors = 4)
         pass
 
     # As far as I can see, we don't have to perform any retraining on the model
@@ -17,6 +20,9 @@ class Classifier:
     
     # Trains on training dataset, the arrays of input <data>, on target, or 
     # output singular integer <target>.
+    #
+    # <data> = [001011101010...]
+    # <target> = 0 or 1 or 2 or 3
     #
     # This happens before classifier is run on the environment.
     def fit(self, data, target):
@@ -30,6 +36,7 @@ class Classifier:
     # ClassifierAgent.py handles output conversion of the number to these 
     # strings taken by the environment. So we may not even use legal at all.
     def predict(self, data, legal=None):
-        print(legal)
+
+        # Currently only outputs 1 (pretty sure that means East). 
         return 1
         
