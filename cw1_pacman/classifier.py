cc5efb003369336310cc3b3a877589ca18be30f9
Killian <killian2100@hotmail.com>
2023-02-14 12:37:45 +0000

    Implementation of Forward Pass
    Finished forward pass implementation. Wasn't too difficult.

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -10,6 +10,7 @@ class Classifier:
     # object scope constants and variables here. We may not even use this.
     def __init__(self):
         self.knn = KNeighborsClassifier(n_neighbors = 4)
+
         pass
 
     # As far as I can see, we don't have to perform any retraining on the model
@@ -26,7 +27,9 @@ class Classifier:
     #
     # This happens before classifier is run on the environment.
     def fit(self, data, target):
-        
+        self.model = MLP(len(data), len(data) + 5, 4)
+        prediction = self.model.forward(data)
+        print(prediction)
         pass
 
     # We simply output the integer output/prediction given a singular feature
@@ -36,7 +39,7 @@ class Classifier:
     # ClassifierAgent.py handles output conversion of the number to these 
     # strings taken by the environment. So we may not even use legal at all.
     def predict(self, data, legal=None):
-
+        
         # Currently only outputs 1 (pretty sure that means East). 
         return 1
         
@@ -48,18 +51,67 @@ class MLP:
         self.input_size = input_size # unaccounted bias
         self.hidden_size = hidden_size # unaccounted bias
         self.output_size = output_size
+        # Input i, output node j.
+        # Note: input and hidden sizes are FLIPPED for easier notation during
+        # calculation later on.
         self.w_j_i = np.random.uniform(low = -1, high = 1,
-                                       size = (self.input_size + 1, 
-                                               self.hidden_size))
+                                       size = (self.hidden_size, 
+                                               self.input_size + 1))
+        
+        # Input node j, output k
+        # Note: hidden and output sizes are FLIPPED for easier notation during
+        # calculation later on.
         self.w_k_j = np.random.uniform(low = -1, high = 1,
-                                       size = (self.hidden_size + 1, 
-                                               self.output_size))
-    
+                                       size = (self.output_size, 
+                                               self.hidden_size + 1))
+
+    # ReLu activation function
+    def activation_ReLu(self, x):
+        return max(0.0, x)
+
+    # Sigmoid activation function
+    def activation_Signmoid(self, x):
+        return 1/(1 + np.exp(-x))
+
+    # Initial part of forward pass, iterates over all inputs for the output
+    # hidden node j. Includes activation function use.
+    def forward_input_j(self, input, j):
+        temp = self.w_j_i[j][0] # w_j_0
+        for i, x in enumerate(input[0]):
+            temp += x * self.w_j_i[j][i + 1]
+        return self.activation_ReLu(temp)
+
+    # 2nd part of forward pass, iterates over all y_j for the output k node z_k.
+    # Includes activation functions use.
+    def forward_hidden_k(self, y, k):
+        temp = self.w_k_j[k][0] # w_k_0
+        for j, y_j in enumerate(y):
+            temp += y_j * self.w_k_j[k][j + 1]
+        return self.activation_Signmoid(temp)
+
     def forward(self, input):
         # z_k = act_func( w_k_0 + sum_over_hidden_nodes( w_k_j * act_func( w_j_0 + sum_over_inputs( w_j_i * x_i ))))
-        temp = w_j_i[0] # w_j_0
-        for i, x in input:
-            temp += x*w_j_i[i+1]
+       
+        y = np.zeros(self.hidden_size)
+        # Calculate all outputs of hidden nodes, y
+        for j, y_j in enumerate(y):
+            y[j] = self.forward_input_j(input, j)
+
+        z = np.zeros(self.output_size)
+        # Calculate all outputs of output nodes, z
+        for k, z_k in enumerate(z):
+            z_k = self.forward_hidden_k(y, k)
+        
+        # Return index of the maximum probability selection.
+        # Can change to perform a random selection, or any other acceptable 
+        # method.
+        return np.argmax(z)
+
+        
+
+            
+        
+            
         
             
             
