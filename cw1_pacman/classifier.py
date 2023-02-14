802609f5470b89746d3ad0d6bfb6d2e0d5597fe5
Killian <killian2100@hotmail.com>
2023-02-14 13:07:44 +0000

    Variable Name Change to Meet Spec

--- cw1_pacman/classifier.py
+++ cw1_pacman/classifier.py
@@ -27,8 +27,8 @@ class Classifier:
     #
     # This happens before classifier is run on the environment.
     def fit(self, data, target):
-        input_size = len(data[0])
-        self.model = MLP(input_size, input_size + 5, 4)
+        inputSize = len(data[0])
+        self.model = MLP(inputSize, inputSize + 5, 4)
         prediction = self.model.forward(data[0])
         
         pass
@@ -38,7 +38,9 @@ class Classifier:
     # 'South', 'West', and 'Stop'.
     #
     # ClassifierAgent.py handles output conversion of the number to these 
-    # strings taken by the environment. So we may not even use legal at all.
+    # strings taken by the environment. Aswell as performing a random action if
+    # our selected move is illegal anyway. So we may not even use legal at all.
+    
     def predict(self, data, legal=None):
         prediction = self.model.forward(data)
         print(prediction)
@@ -49,64 +51,66 @@ class Classifier:
 # Will have singular of both forward and backward passes.
 # Will perform batch back progagation. 
 class MLP:
-    def __init__(self, input_size, hidden_size, output_size = 4):
-        self.input_size = input_size # unaccounted bias
-        self.hidden_size = hidden_size # unaccounted bias
-        self.output_size = output_size
+    def __init__(self, inputSize, hiddenSize, outputSize = 4):
+        self.inputSize = inputSize # unaccounted bias
+        self.hiddenSize = hiddenSize # unaccounted bias
+        self.outputSize = outputSize
         # Input i, output node j.
         # Note: input and hidden sizes are FLIPPED for easier notation during
         # calculation later on.
-        self.w_j_i = np.random.uniform(low = -1, high = 1,
-                                       size = (self.hidden_size, 
-                                               self.input_size + 1))
+        self.weightsJI = np.random.uniform(low = -1, high = 1,
+                                       size = (self.hiddenSize, 
+                                               self.inputSize + 1))
         
         # Input node j, output k
         # Note: hidden and output sizes are FLIPPED for easier notation during
         # calculation later on.
-        self.w_k_j = np.random.uniform(low = -1, high = 1,
-                                       size = (self.output_size, 
-                                               self.hidden_size + 1))
+        self.weightsKJ = np.random.uniform(low = -1, high = 1,
+                                       size = (self.outputSize, 
+                                               self.hiddenSize + 1))
 
     # ReLu activation function
-    def activation_ReLu(self, x):
+    def activationReLu(self, x):
         return max(0.0, x)
 
     # Sigmoid activation function
-    def activation_Signmoid(self, x):
-        return 1/(1 + np.exp(-x))
+    def activationSymSig(self, x):
+        return -1 + 1/(1 + np.exp(-x))
 
     # Initial part of forward pass, iterates over all inputs for the output
     # hidden node j. Includes activation function use.
-    def forward_input_j(self, input, j):
-        temp = self.w_j_i[j][0] # w_j_0
+    def sumInputOnWeights(self, input, j):
+        temp = self.weightsJI[j][0] # w_j_0
         for i, x in enumerate(input):
-            temp += x * self.w_j_i[j][i + 1]
-        return self.activation_ReLu(temp)
+            temp += x * self.weightsJI[j][i + 1]
+        return self.activationReLu(temp)
 
     # 2nd part of forward pass, iterates over all y_j for the output k node z_k.
     # Includes activation functions use.
-    def forward_hidden_k(self, y, k):
-        temp = self.w_k_j[k][0] # w_k_0
+    def sumHiddenOnWeights(self, y, k):
+        temp = self.weightsKJ[k][0] # w_k_0
         for j, y_j in enumerate(y):
-            temp += y_j * self.w_k_j[k][j + 1]
-        return self.activation_Signmoid(temp)
+            temp += y_j * self.weightsKJ[k][j + 1]
+        return self.activationSymSig(temp)
 
     def forward(self, input, train = False):
-        # z_k = act_func( w_k_0 + sum_over_hidden_nodes( w_k_j * act_func( w_j_0 + sum_over_inputs( w_j_i * x_i ))))
+        # z_k = act_func( w_k_0 + sum_over_hidden_nodes( weightsKJ * act_func( w_j_0 + sum_over_inputs( weightsJI * x_i ))))
        
-        y = np.zeros(self.hidden_size)
+        y = np.zeros(self.hiddenSize)
         # Calculate all outputs of hidden nodes, y
         for j, y_j in enumerate(y):
-            y[j] = self.forward_input_j(input, j)
+            y[j] = self.sumInputOnWeights(input, j)
 
-        z = np.zeros(self.output_size)
+        z = np.zeros(self.outputSize)
         # Calculate all outputs of output nodes, z
         for k, z_k in enumerate(z):
-            z_k = self.forward_hidden_k(y, k)
+            z_k = self.sumHiddenOnWeights(y, k)
         
         # Return index of the maximum probability selection.
         # Can change to perform a random selection, or any other acceptable 
         # method.
+        print(z)
+        print(y)
         return np.argmax(z)
         
     
